9ba662f04242e50ff59409ce25a8fc777bd143ca
ryanessam <ryan.essam@sainsburys.co.uk>
2021-02-16 12:16:06 +0000

    another change

--- mysql_app.py
+++ mysql_app.py
@@ -9,7 +9,6 @@ user = os.environ.get("mysql_user")
 password = os.environ.get("mysql_pass")
 database = os.environ.get("mysql_db")
 
-
 # Establish a database connection
 connection = pymysql.connect(
   host,
