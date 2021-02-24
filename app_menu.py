7207895e93a02f461148419d4a1d916ff0b82584
ryanessam <ryan.essam@sainsburys.co.uk>
2021-02-24 15:08:34 +0000

    wednesday changes

--- app_menu.py
+++ app_menu.py
@@ -17,13 +17,13 @@ Please select an option by entering a number
 1. Product Menu 
 2. Courier Menu 
 3. Order Menu
-0. Exit''') 
+0. Exit ''') 
 
     if menu == '1':
         os.system('clear')
         while True:
             
-            option1 = input ('\n' 'Select Option' '\n' '\n1. Products List' '\n2. Add a Product' '\n3. Update a Product'  '\n4. Remove a Product' '\n0. Main Menu')
+            option1 = input ('\n' 'Select Option' '\n' '\n1. Products List' '\n2. Add a Product' '\n3. Update a Product'  '\n4. Remove a Product' '\n0. Main Menu ')
             
             if option1 == '1':
                 os.system('clear')
@@ -41,7 +41,7 @@ Please select an option by entering a number
 
             elif option1 == '3':
                 os.system('clear')
-                update_product = input ('Select Option' '\n1. List of Products' '\n0. Cancel')
+                update_product = input ('Select Option' '\n1. List of Products' '\n0. Cancel ')
                 if update_product == '1':
                     mysql_app.update_entry('product', 'product_price', mysql_app.connection)
                     app_functions.yesno()
@@ -71,7 +71,7 @@ Please select an option by entering a number
         os.system('clear')
         while True:
 
-            option2 = input ('\nSelect Option' '\n' '\n1. Current Couriers' '\n2. Create New Courier' '\n3. Update Existing Courier' '\n4. Remove a Courier' '\n0. Main Menu')
+            option2 = input ('\nSelect Option' '\n' '\n1. Current Couriers' '\n2. Create New Courier' '\n3. Update Existing Courier' '\n4. Remove a Courier' '\n0. Main Menu ')
 
             if option2 == '1':
                 os.system('clear')
@@ -89,7 +89,7 @@ Please select an option by entering a number
 
             elif option2 == '3':
                 os.system('clear')
-                update_courier = input ('Select Option' '\n1. List of Couriers' '\n0. Cancel')
+                update_courier = input ('Select Option' '\n1. List of Couriers' '\n0. Cancel ')
                 if update_courier == '1':
                     mysql_app.update_entry('courier', 'contact_number', mysql_app.connection)
                     app_functions.yesno()
@@ -119,11 +119,12 @@ Please select an option by entering a number
         os.system('clear')
         while True:
 
-            option3 = input('\n' 'Select Option' '\n' '\n1. Print Orders to Screen' '\n2. Create New Order' '\n3. Update Order Status' '\n4. Edit an Order' '\n5. Delete an Order' '\n0. Main Menu')
+            option3 = input('\n' 'Select Option' '\n' '\n1. Print Orders to Screen' '\n2. Create New Order' '\n3. Update Order Status' '\n4. Edit an Order' '\n5. Delete an Order' '\n0. Main Menu ')
 
             if option3 == '1':
                 os.system('clear')
                 mysql_app.print_order()
+                mysql_app.view_order()
                 app_functions.yesno()
                 os.system('clear')
                 continue
@@ -137,7 +138,7 @@ Please select an option by entering a number
 
             elif option3 == '3':
                 os.system('clear')
-                update_menu = input ('Select Option' '\n1. List of Orders' '\n0. Cancel')
+                update_menu = input ('Select Option' '\n1. List of Orders' '\n0. Cancel ')
                 
                 if update_menu == '1':
                     mysql_app.update_order_status(mysql_app.connection)
@@ -151,7 +152,7 @@ Please select an option by entering a number
                     
             elif option3 == '4':
                 os.system('clear')
-                update_order = input ('Select Option' '\n1. List of Orders' '\n0. Cancel')
+                update_order = input ('Select Option' '\n1. List of Orders' '\n0. Cancel ')
 
                 if update_order == '0':
                     os.system('clear')
--- mysql_app.py
+++ mysql_app.py
@@ -31,7 +31,7 @@ def print_table(item, number):
 def add_entry(item, number, connection):
     print_table(item, number)
     name = input (f'Insert new {item} name ').title()
-    value = input (f'Insert {number} for {name}?')
+    value = input (f'Insert {number} for {name}? ')
     execute_sql(connection, f'INSERT INTO {item}s ({item}_name, {number}) VALUES ("{name}", "{value}")')
 
 
@@ -39,7 +39,7 @@ def update_entry(item, number, connection):
     print_table(item, number)
     existing_ids = get_ids(item)
     while True:
-        ID = input (f'Select which {item} to Update')
+        ID = input (f'Select which {item} to Update ')
         try:
             selected_id = int(ID)
         except ValueError:
@@ -52,13 +52,13 @@ def update_entry(item, number, connection):
         
         else: 
     
-            name = input (f'Insert new {item} name or leave blank to skip').title()
+            name = input (f'Insert new {item} name or leave blank to skip ').title()
             if name == '':
                 pass
             else:
                 execute_sql(connection, f'UPDATE {item}s SET {item}_name = "{name}" WHERE {item}_ID = "{ID}";')
             
-            value = input (f'Insert new {number} or leave blank to skip?')
+            value = input (f'Insert new {number} or leave blank to skip? ')
             if value == '':
                 pass
             else:
@@ -74,7 +74,7 @@ def delete_entry(item, connection):
     
     existing_ids = get_ids(item)
     while True:
-        deletion = input (f'Select the ID of the {item} you would like to delete?')
+        deletion = input (f'Select the ID of the {item} you would like to delete? ')
         try:
             selected_id = int(deletion)
         except ValueError:
@@ -95,17 +95,42 @@ def print_order():
     cursor.execute(f'SELECT order_ID, name, address, phone_number, courier_ID, status FROM orders')
     mytable = prettytable.from_db_cursor(cursor)
     print (mytable)
+    
+    
+def view_order():
+    existing_ids = get_ids('order')
+    while True:
+        basket = input("Which Order would you like to view?")
+        try:
+            selected_id = int(basket)
+        except ValueError:
+            print ('Order must be selected as a number')
+            continue
+
+        if int(selected_id) not in existing_ids:
+            print ("Invalid entry, please try again")
+            continue
+        else:
+            current_basket = execute_sql_select(connection, f'SELECT o.name, p.product_name FROM basket b JOIN orders o ON b.order_ID = o.order_ID JOIN products p ON b.product_ID = p.product_ID WHERE o.order_ID = "{selected_id}";')          
+            products_list = []
+            for row in current_basket:
+                products_list.append(row[1])
+            
+            print('Here is your current order')
+            for product in products_list:
+                print(product)
+            break
 
 
 def add_order(connection):
-    user_name = input ('Insert your name here').title()
-    user_address = input ('Insert your address here').title()
-    user_phone_number = input ('What is your contact number?')
+    user_name = input ('Insert your name here ').title()
+    user_address = input ('Insert your address here ').title()
+    user_phone_number = input ('What is your contact number? ')
     
     print_table('courier', 'contact_number')
     existing_ids = get_ids('courier')
     while True:
-        courier = input('Please select the number of the courier you would like to use')
+        courier = input('Please select the number of the courier you would like to use ')
         try:
             selected_id = int(courier)
         except ValueError:
@@ -128,7 +153,7 @@ def add_order(connection):
 
     execute_sql(connection, f'INSERT INTO orders (name, address, phone_number, courier_ID, status) VALUES ("{user_name}", "{user_address}", "{user_phone_number}", "{user_courier}", "{user_status}" );')
 
-    order_ID = execute_sql_select(connection, "select MAX(order_ID) from orders")[0][0]
+    order_ID = execute_sql_select(connection, "SELECT MAX(order_ID) from orders")[0][0]
     for item in items:
             execute_sql(connection, f"INSERT INTO basket (order_ID, product_ID) values ('{order_ID}', '{item}');")
            
@@ -137,7 +162,7 @@ def update_order_status(connection):
     print_order()
     existing_ids = get_ids('order')
     while True:
-        update_status = input('Which order would you like to update?')
+        update_status = input('Which order would you like to update? ')
         try:
             selected_id = int(update_status)
         except ValueError:
@@ -153,7 +178,7 @@ def update_order_status(connection):
             status_loop = True
             order_status = order_status = ["Preparing", "Ready", "With Courier", "Delivered"] 
             while status_loop:
-                status_question = input("Is this order preparing, ready, with courier or delivered?").title()
+                status_question = input("Is this order preparing, ready, with courier or delivered? ").title()
                 if status_question in order_status:
                     execute_sql(connection, f'UPDATE orders SET status = "{status_question}" WHERE order_ID = "{selected_id}";')
                     status_loop = False
@@ -167,7 +192,7 @@ def edit_order(connection):
     while True:
         existing_ids = get_ids('order')
         print_order()
-        deletion = input (f'Select the ID of the order you would like to edit or press 0 to cancel?')
+        deletion = input (f'Select the ID of the order you would like to edit or press 0 to cancel? ')
         
         if deletion == '0':
             os.system('clear')
@@ -186,25 +211,25 @@ def edit_order(connection):
         
         else:
             while True:
-                question = input ('\n' 'Select Option' '\n' '\n1. Edit Personal Information' '\n2. Edit Courier' '\n3. Edit Products' '\n0. Exit')
+                question = input ('\n' 'Select Option' '\n' '\n1. Edit Personal Information' '\n2. Edit Courier' '\n3. Edit Products' '\n0. Exit ')
 
                 if question == '0':
                     break
                 
                 if question == '1':
-                    customer_name = input ('Type new name or leave blank to skip').title()
+                    customer_name = input ('Type new name or leave blank to skip ').title()
                     if customer_name == '':
                         pass
                     else:
                         execute_sql(connection, f'UPDATE orders SET name = "{customer_name}" WHERE order_ID = "{selected_id}";')
                         
-                    customer_address = input ('Type new address or leave blank to skip').title()
+                    customer_address = input ('Type new address or leave blank to skip ').title()
                     if customer_address == '':
                         pass
                     else:
                         execute_sql(connection, f'UPDATE orders SET name = "{customer_address}" WHERE order_ID = "{selected_id}";')
                     
-                    customer_phone = input ('Type new number or leave blank to skip')
+                    customer_phone = input ('Type new number or leave blank to skip ')
                     if customer_phone == '':
                         pass
                     else:
@@ -214,7 +239,7 @@ def edit_order(connection):
                     existing_ids = get_ids('courier')
                     print_table('courier', 'contact_number')
                     while True:
-                        new_courier = input("Select the number of the courier you would like to use")
+                        new_courier = input("Select the number of the courier you would like to use ")
 
                         try:
                             selected_courier_id = int(new_courier)
@@ -230,15 +255,42 @@ def edit_order(connection):
                             execute_sql(connection, f'UPDATE orders SET courier_ID = "{selected_courier_id}" WHERE order_ID = "{selected_id}";')
                             break
 
-                # if question == '3':           
+                if question == '3': 
 
-# option 3 edit basket
+                    current_basket = execute_sql_select(connection, f'SELECT o.name, p.product_name FROM basket b JOIN orders o ON b.order_ID = o.order_ID JOIN products p ON b.product_ID = p.product_ID WHERE o.order_ID = "{selected_id}";')          
+                    products_list = []
+                    for row in current_basket:
+                        products_list.append(row[1])
+                    
+                    print('Here is your current order')
+                    for product in products_list:
+                        print(product)
+                    
+                    while True:
+                        basket_question = input('Leave blank to keep this order or press 0 to delete this order and create a new one ')
+                        if basket_question == '':
+                            os.system('clear')
+                            break
+                        
+                        elif basket_question == '0':
+                            os.system('clear')
+                            execute_sql(connection, f'DELETE FROM basket WHERE order_ID = {selected_id}')
+                            items = list_of_ordered_products()
+                            order_ID = selected_id
+                            for item in items:
+                                execute_sql(connection, f"INSERT INTO basket (order_ID, product_ID) values ('{order_ID}', '{item}');")
+                            break
+                        
+                        else:
+                            print('Invalid entry please try again')
+                            continue
+
 
 def delete_order(connection):
     print_order()
     existing_ids = get_ids('order')
     while True:
-        deletion = input (f'Select the ID of the order you would like to delete?')
+        deletion = input (f'Select the ID of the order you would like to delete? ')
         try:
             selected_id = int(deletion)
         except ValueError:
@@ -280,7 +332,7 @@ def list_of_ordered_products():
     existing_ids = get_ids('product')
     ordered_products = []
     while True:
-        ordered_product = input ("From the list of available Products please select the number of a product you would like to add. When you are finished press 0")
+        ordered_product = input ("From the list of available Products please select the number of a product you would like to add. When you are finished press 0 ")
         try:
             selected_id = int(ordered_product)
         except ValueError:
@@ -297,7 +349,3 @@ def list_of_ordered_products():
             ordered_products.append(selected_id)
     
     return ordered_products
-
-
-
-
